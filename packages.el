175ac54e04f0037e98812998d6b4beb9873de586
Bryan McCoid <bryan.mccoid@gmail.com>
2020-03-10 21:13:52 -0700

    remove boilerplate, fix authorship, etc..

--- packages.el
+++ packages.el
@@ -2,62 +2,18 @@
 ;;
 ;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
 ;;
-;; Author: bryan mccoid <bryan@bryan-dev-machine>
-;; URL: https://github.com/syl20bnr/spacemacs
+;; Author: bryan mccoid <bryan.mccoid@gmail.com>
+;; URL: https://github.com/bryandmc/ivy-posframe-layer
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
-;;; License: GPLv3
+;;; License: MIT
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `ivy-posframe-layer-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `ivy-posframe-layer/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `ivy-posframe-layer/pre-init-PACKAGE' and/or
-;;   `ivy-posframe-layer/post-init-PACKAGE' to customize the package as it is loaded.
 
 ;;; Code:
-
 (defconst ivy-posframe-layer-packages
   '(ivy-posframe)
-  "The list of Lisp packages required by the ivy-posframe-layer layer.
-
-Each entry is either:
-
-1. A symbol, which is interpreted as a package to be installed, or
-
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
-
-    The following keys are accepted:
-
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
-
-    - :location: Specify a custom installation location.
-      The following values are legal:
-
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
-
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
-
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format"
-  )
+  "The list of Lisp packages required by the ivy-posframe-layer layer.")
 
 (setq ivy-posframe-layer-packages '(ivy-posframe))
 
@@ -69,7 +25,5 @@ Each entry is either:
 
   (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
   (ivy-posframe-mode 1))
-
-
 
 ;;; packages.el ends here
